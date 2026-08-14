3089e4035d4c8d46d4691df3f6e2c0020410fb09
arturviannaa <arturvianna@icloud.com>
2026-08-14 02:08:50 -0300

    chore: bootstrap do backend em Spring Boot 3.5 e Java 21
    Reescrita do controle-gastos-residenciais, que tinha o backend em
    C#/.NET. O contrato da API e mantido igual para o frontend existente
    continuar funcionando sem alteracao.

---
+++ src/main/java/tech/pervian/gastos/GastosApplication.java
@@ -0,0 +1,12 @@
+package tech.pervian.gastos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GastosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GastosApplication.class, args);
+    }
+}
